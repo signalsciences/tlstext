b03af078574c50f9e287e1d233f34c45e14127d6
Nick Galbreath <nickg@nickg13.local>
2015-01-07 19:35:28 +0900

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tlstext
+
 Simple mapping of TLS Versions and Cipher Suites to Strings
+
+The [Go TLS cipher suites and TLS versions](http://golang.org/pkg/crypto/tls/#pkg-constants) are untyped or `uint16` with a string representation.
+
+This package provides simple functions `VersionText` and
+`CipherSuiteText` that provide the raw value to string translations.
+
---
+++ tlstext.go
@@ -0,0 +1,47 @@
+package tlstext
+
+// This package provides simple functions `VersionText` and
+// `CipherSuiteText` that provide the raw value to string translations.
+
+// reverse map of binary TLS Version to string
+var versionMap = map[uint16]string{
+	0x0300: "SSL30",
+	0x0301: "TLS10",
+	0x0302: "TLS11",
+	0x0303: "TLS12",
+}
+
+// reverse map of binary TLS Cipher suite to string
+var cipherMap = map[uint16]string{
+	0x0005: "TLS_RSA_WITH_RC4_128_SHA",
+	0x000a: "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
+	0x002f: "TLS_RSA_WITH_AES_128_CBC_SHA",
+	0x0035: "TLS_RSA_WITH_AES_256_CBC_SHA",
+	0xc007: "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
+	0xc009: "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+	0xc00a: "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+	0xc011: "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
+	0xc012: "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA",
+	0xc013: "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+	0xc014: "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+	0xc02f: "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+	0xc02b: "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+}
+
+// TLSVersionString maps a TLS version to a string, or "unknown"
+func Version(x uint16) string {
+	v, ok := versionMap[x]
+	if !ok {
+		return "unknown"
+	}
+	return v
+}
+
+// CipherSuiteText maps a TLS Cipher Suite to a string or "unknown"
+func CipherSuite(x uint16) string {
+	v, ok := cipherMap[x]
+	if !ok {
+		return "unknown"
+	}
+	return v
+}
