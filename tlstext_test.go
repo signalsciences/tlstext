637b5e54043d441d92dde0956ef53b366e2e17d6
Nick Galbreath <nickg@nickg13.local>
2015-01-07 19:45:40 +0900

    gofmt

--- tlstext_test.go
+++ tlstext_test.go
@@ -8,14 +8,14 @@ func TestCipherSuite(t *testing.T) {
 	value := uint16(0xc02b)
 	expected := "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
 	actual := CipherSuite(value)
-	if expected != actual  {
+	if expected != actual {
 		t.Errorf("For %d, expected %q got %q", value, expected, actual)
 	}
 
 	value = uint16(1234)
 	expected = ""
 	actual = CipherSuite(value)
-	if expected != actual  {
+	if expected != actual {
 		t.Errorf("For %d, expected %q got %q", value, expected, actual)
 	}
 }
@@ -24,14 +24,14 @@ func TestVersion(t *testing.T) {
 	value := uint16(0x0303)
 	expected := "TLS12"
 	actual := Version(value)
-	if expected != actual  {
+	if expected != actual {
 		t.Errorf("For %d, expected %q got %q", value, expected, actual)
 	}
 
 	value = uint16(1234)
 	expected = ""
 	actual = Version(value)
-	if expected != actual  {
+	if expected != actual {
 		t.Errorf("For %d, expected %q got %q", value, expected, actual)
 	}
 }
